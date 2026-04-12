7e645bcd677f64ad9ab87bf09bb0d2a38ae2c6c7
zekeriyabasan <zekeriyabasan@gmail.com>
2026-04-12 15:18:58 +0300

    use tags

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ from starlette import status
 
 zekapi = FastAPI()
 
-@zekapi.get('/main')
+@zekapi.get('/main', tags=['main'])
 def index():
     return {"text":"Hello ZEK !"}
 
-@zekapi.delete('/{id}')
+@zekapi.delete('/{id}', tags=['product-remove'])
 def delete(id):
     return{"id":id}
 
-@zekapi.get('/products/all') # IMPORTANT path conflict all like id thinking so should be before then '/products/{id}' get_product_by_id method
+@zekapi.get('/products/all', tags=['product']) # IMPORTANT path conflict all like id thinking so should be before then '/products/{id}' get_product_by_id method
 def get_all_product():
     return [{'id':1,'name':'Flybikes'},{'id':2,'name':'Federalbikes'}]   
 
-@zekapi.get('/products/{id}')
+@zekapi.get('/products/{id}', tags=['product'])
 def get_product_by_id(id: int): # id must be integer pydantic
     return {'id':id,'name':'Flybikes'}
 
@@ -35,20 +35,20 @@ class Product:
     price: float
     stock: int
 
-@zekapi.get('/products/type/{type}')
+@zekapi.get('/products/type/{type}', tags=['product', 'product-type'])
 def get_product_by_type(type: ProductType):
     return {'id':2, 'name':f"Product {type}", 'type':type}
 
-@zekapi.get('/products/{id}/filter/{type}')
+@zekapi.get('/products/{id}/filter/{type}', tags=['product','product-type'])
 def get_product_by_filter(id:int, type: ProductType, name:str, max_price:Optional[float], min_price:float = 0): # default par must be last parameter
     return {'message':f"your data has id {id} - type {type} - name {name} min_price {min_price} - max_price {max_price}"}
 
-@zekapi.post('/products')
+@zekapi.post('/products', tags=['product'])
 def create_a_product(product:Product, response:Response):
     response.status_code = status.HTTP_201_CREATED
     return {'product':product}
 
-@zekapi.put('/products/{id}')
+@zekapi.put('/products/{id}', tags=['product'])
 def update_a_product(id:int, product:Product, response:Response):
     if(id > 10) :
         response.status_code = status.HTTP_404_NOT_FOUND

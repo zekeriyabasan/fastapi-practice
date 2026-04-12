05fec12e4118fbcb0d912ebe1f0a7ef35ed84a61
zekeriyabasan <zekeriyabasan@gmail.com>
2026-04-12 15:04:41 +0300

    use status codes and Response param into the method from fastapi

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from dataclasses import dataclass
 from enum import Enum
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from starlette import status
 
 zekapi = FastAPI()
 
@@ -26,6 +28,13 @@ class ProductType(str, Enum):
     drink = "drink"
     car = "car"
 
+@dataclass
+class Product:
+    id: int
+    name: str
+    price: float
+    stock: int
+
 @zekapi.get('/products/type/{type}')
 def get_product_by_type(type: ProductType):
     return {'id':2, 'name':f"Product {type}", 'type':type}
@@ -33,6 +42,19 @@ def get_product_by_type(type: ProductType):
 @zekapi.get('/products/{id}/filter/{type}')
 def get_product_by_filter(id:int, type: ProductType, name:str, max_price:Optional[float], min_price:float = 0): # default par must be last parameter
     return {'message':f"your data has id {id} - type {type} - name {name} min_price {min_price} - max_price {max_price}"}
+
+@zekapi.post('/products')
+def create_a_product(product:Product, response:Response):
+    response.status_code = status.HTTP_201_CREATED
+    return {'product':product}
+
+@zekapi.put('/products/{id}')
+def update_a_product(id:int, product:Product, response:Response):
+    if(id > 10) :
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'message':f'not found product {product}'}
+    
+    response.status_code = status.HTTP_204_NO_CONTENT
 
 
 

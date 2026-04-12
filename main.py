5a94a86b1c87e56d4f62574917cd220b3c4410a0
zekeriyabasan <zekeriyabasan@gmail.com>
2026-04-12 12:50:01 +0300

    path sorting and use path parameter

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ def index():
 @zekapi.delete('/{id}')
 def delete(id):
     return{"id":id}
+
+@zekapi.get('/products/all') # IMPORTANT path conflict all like id thinking so should be before then '/products/{id}' get_product_by_id method
+def get_all_product():
+    return [{'id':1,'name':'Flybikes'},{'id':2,'name':'Federalbikes'}]   
+
+@zekapi.get('/products/{id}')
+def get_product_by_id(id: int): # id must be integer pydantic
+    return {'id':id,'name':'Flybikes'}
+
+
+

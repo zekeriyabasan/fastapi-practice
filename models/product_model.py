3d9a884d96610314b8a2bcd98199db93c5d40abe
zekeriyabasan <zekeriyabasan@gmail.com>
2026-04-17 19:32:15 +0300

    use Body field validators

--- models/product_model.py
+++ models/product_model.py
@@ -1,3 +1,4 @@
+from fastapi import Body
 from pydantic import BaseModel
 
 
@@ -6,3 +7,9 @@ class Product(BaseModel):
     name: str
     price: float
     stock: int
+    comment: str = Body(
+    ..., # ... bu alanın zorunlu required olduğunu söyler
+    min_length=10,
+    max_length=50,
+    regex='.*[a-zA-ZğüşöçıİĞÜŞÖÇ]+.*' # en az 1 harf içersin
+)

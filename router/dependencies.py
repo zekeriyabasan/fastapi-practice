b7fbb7ca93ddbc23901fa8233a77b4a4c05466f7
zekeriyabasan <zekeriyabasan@gmail.com>
2026-04-29 23:15:42 +0300

    use multi depends

--- router/dependencies.py
+++ router/dependencies.py
@@ -5,12 +5,22 @@ router = APIRouter(
     tags=['dependencies']
 )
 
-def convert_headers(request:Request, seperator:str = "--"):
+def convert_queries(request:Request, seperator:str):
+    out_queries = []
+    for key,value in request.query_params.items():
+        out_queries.append(f"{key}{seperator}{value}")
+
+    return out_queries
+
+def convert_headers(request:Request, seperator:str = "--", out_queries = Depends(convert_queries)):
     out_headers = []
     for key,value in request.headers.items():
         out_headers.append(f"{key}{seperator}{value}")
 
-    return out_headers
+    return {
+        "out_headers":out_headers,
+        "out_queries":out_queries
+    }
 
 @router.get('/')
 def get_items(headers = Depends(convert_headers)): # not have a seperator params but I send this params because depends function has this params
@@ -39,4 +49,11 @@ def create_account(name:str, email:str, password:str, account:Account = Depends(
         "name": account.name,
         "email":account.email,
         "fullname":account.fullname
+    }
+
+@router.get('/multi-depends')
+def get_items_multi_depends(name:str, zek:str, seperator:str="--", headers = Depends(convert_headers)): # not have a seperator params but I send this params because depends function has this params
+    return{
+        "items":["a","b","c"],
+        "headers":headers
     }
